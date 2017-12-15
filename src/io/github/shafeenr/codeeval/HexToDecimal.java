4494d4b99b01da17387ac8dd8300fe6c280dca7d
Shafeen Rahman <shafeenr@tuta.io>
2017-12-15 08:38:20 -0500

    Added more CodeEval challenges

--- src/io/github/shafeenr/codeeval/HexToDecimal.java
+++ src/io/github/shafeenr/codeeval/HexToDecimal.java
@@ -7,7 +7,35 @@ import java.util.Scanner;
 public class HexToDecimal {
 
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(new File(args[0]));
+        //Scanner in = new Scanner(new File(args[0]));
+        Scanner in = new Scanner(System.in);
+
+        while (in.hasNextLine()) {
+            String[] lineBuffer = in.nextLine().toUpperCase().split("");
+            int sum = 0;
+
+            for (int i = 0; i < lineBuffer.length; i++) {
+                if (lineBuffer[i].equals("A")) {
+                    lineBuffer[i] = "10";
+                } else if (lineBuffer[i].equals("B")) {
+                    lineBuffer[i] = "11";
+                } else if (lineBuffer[i].equals("C")) {
+                    lineBuffer[i] = "12";
+                } else if (lineBuffer[i].equals("D")) {
+                    lineBuffer[i] = "13";
+                } else if (lineBuffer[i].equals("E")) {
+                    lineBuffer[i] = "14";
+                } else if (lineBuffer[i].equals("F")) {
+                    lineBuffer[i] = "15";
+                }
+            }
+            
+            for (int x = lineBuffer.length - 1; x > 0; x--) {
+                sum += Integer.parseInt(lineBuffer[x]) * Math.pow(16, lineBuffer.length - 1 - x);
+            }
+            
+            System.out.println(sum);
+        }
     }
 
 }
